338bc59bd10f1551d940f509d8e01181bc0ab6a6
tiberio40 <frenando40@hotmail.com>
2017-05-03 12:48:31 -0500

    Inicio

---
+++ Comunicacion_Datos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comunicacion_Datos.Startup))]
+namespace Comunicacion_Datos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
